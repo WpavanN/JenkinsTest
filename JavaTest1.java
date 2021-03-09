437e03e395a453835544051addec046c9f44d761
WpavanN <pavan.wadhe@gmail.com>
2021-03-10 02:21:06 +1100

    Initial Commit

---
+++ JavaTest1.java
@@ -0,0 +1,31 @@
+public class JavaTest1
+{
+	public static void main(String[] args) 
+	{
+        int n = 5;
+        // outer loop to handle number of rows
+        //  n in this case
+        for (int i=0; i<n; i++)
+        {
+ 
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for (int j=n-i; j>1; j--)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+  
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for (int j=0; j<=i; j++ )
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+  
+            // ending line after each row
+            System.out.println();
+        }
+	}
+}
